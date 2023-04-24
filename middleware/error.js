da0d6137cce0035641d3889ab9bb1fff27e15fa6
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-04-24 16:19:41 -0400

    Changed 500 error message

--- middleware/error.js
+++ middleware/error.js
@@ -2,5 +2,5 @@ const winston = require('winston')
 
 module.exports = function (err, req, res, next){
   winston.error(err.message, err)
-  res.status(500).send('Something went wrong.')
+  res.status(500).send(err.message)
 }
