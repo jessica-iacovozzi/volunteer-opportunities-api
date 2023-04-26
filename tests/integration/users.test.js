92e8fb02a5dc1869c31fca9b238c6b14480b8ef7
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-04-26 15:25:03 -0400

    Created user route test

---
+++ tests/integration/users.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest')
+
+describe('user route', () => {
+  beforeAll(() => {
+    server = require('../../startup/server')
+  })
+  afterAll(async () => {
+    await server.close()
+  })
+
+  const exec = () => {
+    return request(server)
+      .post('/api/users')
+      .send({ name: 'Test Name',
+              email: 'test@gmail.com',
+              password: '123Pass*'
+            })
+  }
+
+  it('should return 400 if email is already registered', async () => {
+    res = await exec()
+
+    expect(res.status).toBe(400)
+  })
+})
