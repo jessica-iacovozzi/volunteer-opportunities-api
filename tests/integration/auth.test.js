85f97c62d4ebd9efdcb8c8f5d56325225a82664b
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-04-26 16:38:37 -0400

    Created 2 more auth tests (All routes tests are min yellow)

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -2,10 +2,11 @@ const request = require('supertest')
 const { Sector } = require('../../models/sector')
 const { User } = require('../../models/user')
 
-describe('auth middleware', () => {
+describe('auth', () => {
   beforeAll(() => {
     server = require('../../startup/server')
     token = new User().generateAuthToken()
+    email = ''
   })
   afterAll(async () => {
     await Sector.collection.deleteOne({ name: 'Test Sector' })
@@ -14,34 +15,60 @@ describe('auth middleware', () => {
 
   let token
 
-  const exec = () => {
+  const postSector = () => {
     return request(server)
       .post('/api/sectors')
       .set('x-api-key', token)
       .send({ name: 'Test Sector' })
   }
 
-  it('should return 401 if no token is provided', async () => {
-    token = ''
-
-    const res = await exec()
-
-    expect(res.status).toBe(401)
-  })
-
-  it('should return 400 if token is invalid', async () => {
-    token = 'a'
-
-    const res = await exec()
-
-    expect(res.status).toBe(400)
-  })
-
-  it('should return 400 if no email was given', async () => {
-    res = await request(server)
+  const getToken = () => {
+    return request(server)
       .post('/api/auth')
-      .send({ email: '', password: 'Abc1234*' })
+      .send({ email,
+              password: 'Abc1234*' })
+  }
 
-    expect(res.status).toBe(400)
+  describe('auth route', () => {
+    it('should return 400 if invalid email is given', async () => {
+      email = 'testing@email.com'
+
+      res = await getToken()
+
+      expect(res.status).toBe(400)
+    })
+
+    it('should return 400 if invalid password is given', async () => {
+      email = 'test@gmail.com'
+
+      res = await getToken()
+
+      expect(res.status).toBe(400)
+    })
+
+    it('should return 400 if no email was given', async () => {
+      res = await getToken()
+
+      expect(res.status).toBe(400)
+    })
+  })
+
+  describe('auth middleware', () => {
+    it('should return 401 if no token is provided', async () => {
+      token = ''
+
+      const res = await postSector()
+
+      expect(res.status).toBe(401)
+    })
+
+    it('should return 400 if token is invalid', async () => {
+      token = 'a'
+
+      const res = await postSector()
+
+      expect(res.status).toBe(400)
+    })
+
   })
 })
