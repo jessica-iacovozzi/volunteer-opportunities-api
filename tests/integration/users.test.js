86cd93bda6c7e0d4fda085b9b686730dc7ea8e6a
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-04-26 20:08:40 -0400

    Created test to get current user

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -1,25 +1,41 @@
 const request = require('supertest')
+const { User } = require('../../models/user')
 
 describe('/api/users', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     server = require('../../startup/server')
   })
-  afterAll(async () => {
+  afterEach(async () => {
+    await User.collection.deleteMany({ name: 'Test Name'})
     await server.close()
   })
 
-  const exec = () => {
-    return request(server)
+  let email
+
+  const post = async () => {
+    return await request(server)
       .post('/api/users')
-      .send({ name: 'Test Name',
-              email: 'test@gmail.com',
+      .send({
+              name: 'Test Name',
+              email,
               password: '123Pass*'
             })
   }
 
   it('should return 400 if email is already registered', async () => {
-    res = await exec()
+    email = 'test@gmail.com'
+
+    const res = await post()
 
     expect(res.status).toBe(400)
+  })
+
+  it('should return user if valid api key is given', async () => {
+    email = 'test2@gmail.com'
+
+    const key = await post()
+    const res = await request(server).get('/api/users/me').set('x-api-key', key.headers['x-api-key'])
+
+    expect(res.body).toHaveProperty('name', 'Test Name')
   })
 })
